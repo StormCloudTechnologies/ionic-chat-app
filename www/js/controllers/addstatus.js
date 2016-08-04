1d37fc15535396c2d66cab0c87e27de49cf10709
Sudarshan101 <sudarshan.vis101@gmail.com>
2016-08-04 14:46:53 +0530

    user status

--- www/js/controllers/addstatus.js
+++ www/js/controllers/addstatus.js
@@ -1,10 +1,28 @@
 angular.module('AddStatus.controllers', [])
 
-.controller('AddStatusCtrl', function($scope, $ionicLoading, $ionicPlatform, $state, localStorageService, APIService) {
+.controller('AddStatusCtrl', function($scope, $localstorage, $ionicLoading, $ionicPlatform, $state, localStorageService, APIService) {
   $ionicPlatform.ready(function(){
     try{
-      
-     }catch(err){
+      	$scope.usernumber = localStorageService.get('usernumber');
+      	$scope.user = {};
+      	$scope.user.status = '';
+      	$scope.addstatus = function(status){
+      		$state.go('status');
+      		APIService.setData({
+	            req_url: url_prefix + 'updateContact',
+	            data: {phone:$scope.usernumber, status: status}
+	        }).then(function(resp) {
+	          console.log(resp);
+	            if(resp.data) {
+	              $localstorage.set("userStatus", resp.data.status);
+	              $scope.user.status = '';
+	              $state.go('status');
+	            }
+	           },function(resp) {
+	            console.log('error',resp);
+	        });
+      	}
+    }catch(err){
       console.log(err.message);
     }
   });
--- www/js/controllers/status.js
+++ www/js/controllers/status.js
@@ -1,11 +1,11 @@
 angular.module('Status.controllers', [])
 
-.controller('StatusCtrl', function($scope, $ionicLoading, $ionicPlatform, $state, localStorageService, APIService, $ionicPopover, $localstorage) {
+.controller('StatusCtrl', function($scope, $ionicLoading, $localstorage, $ionicPlatform, $state, localStorageService, APIService, $ionicPopover, $localstorage) {
   $ionicPlatform.ready(function(){
     try{
-
     	$scope.userStatus = $localstorage.get("userStatus");
-
+    	$scope.usernumber = localStorageService.get('usernumber');
+    	console.log($scope.userStatus);
     	$ionicPopover.fromTemplateUrl('templates/deletestatus.html', {
 		    scope: $scope
 		}).then(function(deletestatus) {
@@ -18,8 +18,20 @@ angular.module('Status.controllers', [])
 		    $scope.deletestatus.hide();
 		};
 
-		$scope.addstatus = function(){
-			$scope.userStatus = "options"
+		$scope.addstatus = function(newStatus){
+			APIService.setData({
+	            req_url: url_prefix + 'updateContact',
+	            data: {phone:$scope.usernumber, status: newStatus}
+	        }).then(function(resp) {
+	          console.log(resp);
+	            if(resp.data) {
+	              $scope.userStatus = resp.data.status;
+	              $localstorage.set("userStatus", resp.data.status);
+	              // $state.go('status');
+	            }
+	           },function(resp) {
+	            console.log('error',resp);
+	        });
 		}
 
       
